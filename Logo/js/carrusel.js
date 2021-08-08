04e7a0af6d42f56d54fcb380afa6ac64400cdfd6
Arias Coding <87912468+Arias-Coding@users.noreply.github.com>
2021-08-08 16:53:26 -0300

    Add files via upload

---
+++ Logo/js/carrusel.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+
+    new Glider(document.querySelector('.carrusel__lista'), {
+        slidesToShow: 1,
+        dots: '.carrusel__indicadores',
+        arrows: {
+            prev: '.carrusel__anterior',
+            next: '.carrusel__siguiente'
+        }
+    });
+
+
+
+})
